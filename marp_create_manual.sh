cf59ce49b532fb14c1ee648b809733558758144e
citrus <citrus.mikan88@gmail.com>
2025-07-28 21:27:49 +0900

    微修正

--- marp_create_manual.sh
+++ marp_create_manual.sh
@@ -1,5 +1,5 @@
 #!/opt/homebrew/bin/fish
 set dir $argv[1]
 set filename $argv[2]
-marp --input-dir $dir --theme ./style/manual.css --pdf --allow-local-files
+marp --theme ./style/manual.css --pdf --allow-local-files $dir/$filename.md
 open $dir/$filename.pdf
